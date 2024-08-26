e3fce3755e2c151d7dae02da5bd71ff49ec1566c
korobkad3v <korobka.d3v@gmail.com>
2024-08-25 19:19:28 -0700

    [+] styles for form

--- src/components/pages/createpostpage/CreatePostPage.js
+++ src/components/pages/createpostpage/CreatePostPage.js
@@ -81,17 +81,25 @@ class CreatePostPage extends Component {
                     <NavBar />
 
                     <div className="create-post">
+                        
                         <form class="create-post__form" onSubmit={this.handleSubmit}>
-                            <h1>New opportunity.</h1>
+                            <h1 className="create-post__title">New opportunity.</h1>
                             <div className="create-post__form-group">
-                                <label htmlFor="title">Title</label>
-                                <input type="text" id="title" name="title"  value={this.state.title} onChange={this.handleChange}/>
+                                <label htmlFor="title" className="create-post__label">Title</label>
+                                <input type="text" 
+                                id="title" 
+                                name="title"
+                                className="create-post__input"  
+                                value={this.state.title} 
+                                onChange={this.handleChange}
+                                placeholder="Enter title"
+                                />
                             </div>
 
                             <div className="create-post__form-group create-post-page__form-group--image-dropzone"
                             onDrop={this.handleImageDrop}
                             onDragOver={(e) => e.preventDefault()}>
-                                <label htmlFor="image"></label>
+                                <label htmlFor="image">Upload Image</label>
                                 <div className="create-post__dropzone">
                                 {this.state.image ? (
                                     <p className="create-post-page__dropzone-text">{this.state.image.name}</p>
@@ -105,7 +113,43 @@ class CreatePostPage extends Component {
                                     style={{ display: 'none' }}
                                     />
                                 </div> 
-                                
+                            </div>
+
+                            <div className="create-post__form-group">
+                                <label htmlFor="email" className="create-post__label">Email </label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    className="create-post__input"
+                                    value={this.state.email}
+                                    onChange={this.handleChange}
+                                    placeholder="Enter your email"
+                                />
+                            </div>
+
+
+                            <div className="create-post__form-group">
+                                <label htmlFor="shortDesc" className="create-post__label">Short Desc</label>
+                                <input type="text" 
+                                id="shortDesc" 
+                                name="shortDesc" 
+                                value={this.state.shortDesc}
+                                onChange={this.handleChange}
+                                className="create-post__input" 
+                                placeholder="Enter a short description"
+                                />
+                            </div>
+                            <div className="create-post__form-group">
+                                <label htmlFor="content" className="create-post__label">Content</label>
+                                <textarea
+                                    id="content"
+                                    name="content"
+                                    value={this.state.content}
+                                    onChange={this.handleChange}
+                                    className="create-post__textarea"
+                                    placeholder="What's on your mind?"
+                                />
                             </div>
                         </form>
                     </div>
